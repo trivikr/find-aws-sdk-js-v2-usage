afc6c598b2018e3d630a21d996e0a470602078ae
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2025-12-07 15:24:04 -0800

    chore: limit webpack chunks to 1

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,9 +1,11 @@
 import { join } from "node:path";
 import TerserPlugin from "terser-webpack-plugin";
+import webpack from "webpack";
 
 const outputPath = join(import.meta.dirname, "src", "utils", "__fixtures__");
 
 export default {
+  target: "node",
   output: {
     path: outputPath,
     libraryTarget: "commonjs2",
@@ -15,4 +17,9 @@ export default {
       }),
     ],
   },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
+  ],
 };
